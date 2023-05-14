f906c2b7f6f30d8292934cdb3e2f9b53fe33e51c
ikrom97 <ikromr04@gmail.com>
2023-05-14 23:05:41 +0500

    makes footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,15 @@
-<footer class="page-footer"></footer>
+<footer class="page-footer">
+  <div class="page-footer__container container">
+    <p class="page-footer__copyright">
+      <span>
+        @lang('copyright') <br>
+        2017 - {{ date('Y') }}
+      </span>
+    </p>
+
+    <p class="page-footer__feedback">
+      @lang('forFeedback') <br>
+      <a href="mailto:info@zafarmirzo.com">info@zafarmirzo.com</a>
+    </p>
+  </div>
+</footer>
