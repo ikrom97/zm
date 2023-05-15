38093be781badd234a706c15662e924058961131
ikrom97 <ikromr04@gmail.com>
2023-05-15 10:08:26 +0500

    makes author page

--- resources/views/pages/author/index.blade.php
+++ resources/views/pages/author/index.blade.php
@@ -1,14 +1,42 @@
 @extends('layouts.app')
 
 @section('links')
-  <link rel="stylesheet" href="{{ asset('css/pages/author/index.min.css') }}">
+  <link
+    rel="stylesheet"
+    href="{{ asset('css/pages/author/index.min.css') }}"
+  >
 @endsection
 
 @section('content')
-  <main class="page-main"></main>
+  <main class="author-page container">
+    <div class="author-page__author">
+      <img
+        class="author-page__image"
+        src="{{ asset('images/author.jpg') }}"
+        width="419"
+        height="421"
+        alt="Зафар Мирзо"
+        loading="lazy"
+      >
+      <q class="author-page__quote">Цель высока, когда любовь обширна</q>
+    </div>
+
+    <h1 class="author-page__title">
+      Авторский сайт <br>
+      Зафар Мирзо
+    </h1>
+
+    <div class="author-page__info">
+      <p class="author-page__info-item">Социальный предприниматель и председатель. Также занимаюсь философским творчеством.</p>
+      <p class="author-page__info-item">1 Мая 1972</p>
+      <p class="author-page__info-item">
+        Из моего собственного философского творчества
+        <a class="button" href="{{ route('home') }}">Мысли</a>
+      </p>
+    </div>
+  </main>
 @endsection
 
 @section('scripts')
   <script src="{{ asset('js/pages/author/index.min.js') }}" type="module"></script>
 @endsection
-
