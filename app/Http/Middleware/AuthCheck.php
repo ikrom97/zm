e1b576819d000c04724e09d0216302f61c596d46
ikrom97 <ikromr04@gmail.com>
2023-05-16 16:33:45 +0500

    post thumb images used instead image

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -22,15 +22,15 @@ class AuthCheck
     }
 
     $response = $next($request);
-    $headers = [
-      'Cache-Control' => 'nocache, no-store, max-age=0, must-revalidate',
-      'Pragma', 'no-cache',
-      'Expires', 'Fri, 01 Jan 1990 00:00:00 GMT',
-    ];
+    // $headers = [
+    //   'Cache-Control' => 'nocache, no-store, max-age=0, must-revalidate',
+    //   'Pragma', 'no-cache',
+    //   'Expires', 'Fri, 01 Jan 1990 00:00:00 GMT',
+    // ];
 
-    foreach ($headers as $key => $value) {
-      $response->headers->set($key, $value);
-    }
+    // foreach ($headers as $key => $value) {
+    //   $response->headers->set($key, $value);
+    // }
 
     return $response;
   }
